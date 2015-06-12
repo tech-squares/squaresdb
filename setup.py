018201a12635ce79abba07fd63103baefa41a5f6
Alex Dehnert <adehnert@mit.edu>
2015-06-12 02:40:02 -0400

    Initial setup.py to allow using with pip/virtualenv

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name = "squaresdb",
+    version = "0.1.2015-06-12",
+    packages = ["squaresdb"],
+    install_requires = [
+        # Server
+        "django",
+        "django-reversion",
+    ],
+
+    author = "Tech Squares webapp team",
+    author_email = "squares-webapps@mit.edu",
+    url = "http://www.mit.edu/~tech-squares/",
+    description = 'MIT Tech Squares membership app',
+    license = "LICENSE.txt",
+
+    keywords = ["mit", ],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 1 - Planning",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+    ],
+)
