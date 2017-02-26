7c9e8b7d41ee08b02dee4ff3c0693bf05e5e9def
Alex Dehnert <adehnert@mit.edu>
2017-02-25 20:47:53 -0500

    Better default dev setup
    - Default config now uses the console email backend, rather than actually
      sending mail
    - Default config now sets up logging (to the console)
    - At the end of the setup script, mentions some next steps (sample DB, create
      user)

--- squaresdb/settings/init-dev.sh
+++ squaresdb/settings/init-dev.sh
@@ -22,3 +22,10 @@ echo; echo
 echo Done! Run the server with
 echo ./manage.py runserver 8007
 echo or similar
+
+echo
+echo "Possible next steps:"
+echo "- Clone the membership database (on Athena: /mit/tech-squares/club-private/signin/git/) and import it:"
+echo "  membership/parsedb.py csv2django --csv membership/club-db/club.csv"
+echo "- Create a superuser account (update email/username appropriately):"
+echo "  ../manage.py createsuperuser --email $USER@example.com --username $USER"
--- squaresdb/settings/local.dev-template.py
+++ squaresdb/settings/local.dev-template.py
@@ -1,3 +1,5 @@
+import os
+
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = something
 
@@ -6,3 +8,25 @@ DEBUG = True
 
 CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+        'squaresdb': {
+            'handlers': ['console'],
+            'level': os.getenv('SQUARESDB_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
