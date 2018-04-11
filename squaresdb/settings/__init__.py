6697dcc528de1152abe3597a32b06f894a8c8ac9
Alex Dehnert <adehnert@mit.edu>
2018-04-11 00:57:57 -0400

    Disable OpenID
    OpenID seems to be rarely used on the internet these days, and doesn't reliably
    prove email ownership, so it's not very useful to us.

--- squaresdb/settings/__init__.py
+++ squaresdb/settings/__init__.py
@@ -82,7 +82,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 AUTHENTICATION_BACKENDS = (
-    'social_core.backends.open_id.OpenIdAuth',
+    #'social_core.backends.open_id.OpenIdAuth',
     #'social_core.backends.google.GoogleOpenId',
     'social_core.backends.google.GoogleOAuth2',
     'social_core.backends.saml.SAMLAuth',
